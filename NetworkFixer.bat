b7c5a9baf8e14a4059e686c944f905e8408ae699
NamNam0w0 <99223863+NamNam0w0@users.noreply.github.com>
2022-02-26 11:20:52 -0600

    Finished making Network Fixer!
    I am not sure if I will add anything else to it.

---
+++ NetworkFixer.bat
@@ -0,0 +1,128 @@
+//// This Multi Tool Created by HG4U_Terminal
+
+cls
+@echo off
+cls
+ping localhost -n 1 >nul
+ping localhost -n 1 >nul
+ping localhost -n 1 >nul
+ping localhost -n 1 >nul
+ping localhost -n 1 >nul
+ping localhost -n 1 >nul
+ping localhost -n 1 >nul
+ping localhost -n 1 >nul
+ping localhost -n 1 >nul
+ping localhost -n 1 >nul
+ping localhost -n 1 >nul
+ping localhost -n 1 >nul
+ping localhost -n 1 >nul
+echo [0;35mN
+title N
+ping localhost -n 1 >nul
+cls
+echo Na
+title Na
+ping localhost -n 1 >nul
+cls
+echo Nam
+title Nam
+ping localhost -n 1 >nul
+cls
+echo NamN
+title NamN
+ping localhost -n 1 >nul
+cls
+echo NamNa
+title NamNa
+ping localhost -n 1 >nul
+cls
+echo NamNam
+title NamNam
+ping localhost -n 1 >nul
+cls
+echo NamNam0
+title NamNam0
+ping localhost -n 1 >nul
+cls
+echo NamNam0w
+title NamNam0w
+ping localhost -n 1 >nul
+cls
+echo NamNam0w0
+title NamNam0w0
+ping localhost -n 1 >nul
+cls
+echo NamNam0w0 
+title NamNam0w0 
+ping localhost -n 1 >nul
+cls
+echo NamNam0w0 l
+title NamNam0w0 l
+ping localhost -n 1 >nul
+cls
+echo NamNam0w0 lo
+title NamNam0w0 lo
+ping localhost -n 1 >nul
+cls
+echo NamNam0w0 lov
+title NamNam0w0 lov
+ping localhost -n 1 >nul
+cls
+echo NamNam0w0 love
+title NamNam0w0 love
+ping localhost -n 1 >nul
+cls
+echo NamNam0w0 loves 
+title NamNam0w0 loves 
+ping localhost -n 1 >nul
+cls
+echo NamNam0w0 loves y
+title NamNam0w0 loves y
+ping localhost -n 1 >nul
+cls
+echo NamNam0w0 loves yo
+title NamNam0w0 loves yo
+ping localhost -n 1 >nul
+cls
+echo NamNam0w0 loves you 
+title NamNam0w0 loves you 
+ping localhost -n 1 >nul
+cls
+echo NamNam0w0 loves you ^<
+title NamNam0w0 loves you ^<
+ping localhost -n 1 >nul
+cls
+echo NamNam0w0 loves you ^<3
+title NamNam0w0 loves you ^<3
+ping localhost -n 2.5 >nul
+cls
+chcp 65001 >nul
+echo.
+echo - ███╗   ██╗ █████╗ ███╗   ███╗███╗   ██╗ █████╗ ███╗   ███╗ ██████╗ ██╗    ██╗ ██████╗ 
+echo - ████╗  ██║██╔══██╗████╗ ████║████╗  ██║██╔══██╗████╗ ████║██╔═████╗██║    ██║██╔═████╗
+echo - ██╔██╗ ██║███████║██╔████╔██║██╔██╗ ██║███████║██╔████╔██║██║██╔██║██║ █╗ ██║██║██╔██║
+echo - ██║╚██╗██║██╔══██║██║╚██╔╝██║██║╚██╗██║██╔══██║██║╚██╔╝██║████╔╝██║██║███╗██║████╔╝██║
+echo - ██║ ╚████║██║  ██║██║ ╚═╝ ██║██║ ╚████║██║  ██║██║ ╚═╝ ██║╚██████╔╝╚███╔███╔╝╚██████╔╝
+echo - ╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝  ╚══╝╚══╝  ╚═════╝ 
+echo - ╭─────────────────────── ♰
+echo - │ ^<3 ¦ ➫ Make sure you ran this in Administator
+echo - │ ^<3 ¦ ➫ This tool was made by NamNam0w0#0001 join my discord ^> https://discord.gg/PkTfxvSjvr
+echo - │ ^<3 ¦ ➫ This tool will reset inernet and flush dns etc
+echo - ╰─────────────────────── ♰
+echo - Press Enter When Ready.
+echo.
+pause
+
+netsh int ip reset C:\resetlog.txt
+
+pause
+
+netsh winsock reset
+
+pause
+
+ipconfig /flushdns
+
+pause
+
+exit
