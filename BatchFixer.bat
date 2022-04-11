9b5282ca05793ac4f1784271c42278dc992ea28f
NamNam0w0 <99223863+NamNam0w0@users.noreply.github.com>
2022-04-11 16:10:02 -0500

    Update
    Fixed a few issues and separated the menu text from the actual batch so it prints in a much more simpler way.

--- BatchFixer.bat
+++ BatchFixer.bat
@@ -1,125 +1,16 @@
 //// This Multi Tool Created by NamNam0w0
-
-cls
 @echo off
-cls
-ping localhost -n 1 >nul
-ping localhost -n 1 >nul
-ping localhost -n 1 >nul
-ping localhost -n 1 >nul
-ping localhost -n 1 >nul
-ping localhost -n 1 >nul
-ping localhost -n 1 >nul
-ping localhost -n 1 >nul
-ping localhost -n 1 >nul
-ping localhost -n 1 >nul
-ping localhost -n 1 >nul
-ping localhost -n 1 >nul
-ping localhost -n 1 >nul
-echo [0;35mN
-title N
-ping localhost -n 1 >nul
-cls
-echo Na
-title Na
-ping localhost -n 1 >nul
-cls
-echo Nam
-title Nam
-ping localhost -n 1 >nul
-cls
-echo NamN
-title NamN
-ping localhost -n 1 >nul
-cls
-echo NamNa
-title NamNa
-ping localhost -n 1 >nul
-cls
-echo NamNam
-title NamNam
-ping localhost -n 1 >nul
-cls
-echo NamNam0
-title NamNam0
-ping localhost -n 1 >nul
-cls
-echo NamNam0w
-title NamNam0w
-ping localhost -n 1 >nul
-cls
-echo NamNam0w0
-title NamNam0w0
-ping localhost -n 1 >nul
-cls
-echo NamNam0w0 
-title NamNam0w0 
-ping localhost -n 1 >nul
-cls
-echo NamNam0w0 l
-title NamNam0w0 l
-ping localhost -n 1 >nul
-cls
-echo NamNam0w0 lo
-title NamNam0w0 lo
-ping localhost -n 1 >nul
-cls
-echo NamNam0w0 lov
-title NamNam0w0 lov
-ping localhost -n 1 >nul
-cls
-echo NamNam0w0 love
-title NamNam0w0 love
-ping localhost -n 1 >nul
-cls
-echo NamNam0w0 loves 
-title NamNam0w0 loves 
-ping localhost -n 1 >nul
-cls
-echo NamNam0w0 loves y
-title NamNam0w0 loves y
-ping localhost -n 1 >nul
-cls
-echo NamNam0w0 loves yo
-title NamNam0w0 loves yo
-ping localhost -n 1 >nul
-cls
-echo NamNam0w0 loves you 
-title NamNam0w0 loves you 
-ping localhost -n 1 >nul
-cls
-echo NamNam0w0 loves you ^<
-title NamNam0w0 loves you ^<
-ping localhost -n 1 >nul
-cls
-echo NamNam0w0 loves you ^<3
+@rem
 title NamNam0w0 loves you ^<3
-ping localhost -n 2.5 >nul
-cls
 chcp 65001 >nul
+cls
 :Main
+color 04
+type MenuText.txt
+rem this prints the option menu for people to see the options
 echo.
-echo - ███╗   ██╗ █████╗ ███╗   ███╗███╗   ██╗ █████╗ ███╗   ███╗ ██████╗ ██╗    ██╗ ██████╗ 
-echo - ████╗  ██║██╔══██╗████╗ ████║████╗  ██║██╔══██╗████╗ ████║██╔═████╗██║    ██║██╔═████╗
-echo - ██╔██╗ ██║███████║██╔████╔██║██╔██╗ ██║███████║██╔████╔██║██║██╔██║██║ █╗ ██║██║██╔██║
-echo - ██║╚██╗██║██╔══██║██║╚██╔╝██║██║╚██╗██║██╔══██║██║╚██╔╝██║████╔╝██║██║███╗██║████╔╝██║
-echo - ██║ ╚████║██║  ██║██║ ╚═╝ ██║██║ ╚████║██║  ██║██║ ╚═╝ ██║╚██████╔╝╚███╔███╔╝╚██████╔╝
-echo - ╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝  ╚══╝╚══╝  ╚═════╝ 
-echo - ╭─────────────────────── ♰
-echo - │ ^<3 ¦ ➫ Make sure you ran this in Administator
-echo - │ ^<3 ¦ ➫ This tool was made by NamNam0w0#0001 join my discord ^> https://discord.gg/PkTfxvSjvr
-echo - │ ^<3 ¦ ➫ This tool will reset inernet and flush dns etc
-echo - ╰─────────────────────── ♰
-echo -
-echo ^> Say the number corresponding to the option to proceed to each option (say the number on the left for example 1 goes to tracking list)
-echo -
-echo ^> 1 To Attempt a Fix for Network Related Issues
-echo -
-echo ^> 2 To Attempt a Fix for Image Cleanup Stuff
-echo -
-echo ^> 3 To Attempt a Fix for ^| Under Construction
-echo.
-set /p ans="//0w0:"
+set /p ans="//0w0 ✞ >"
+rem this sets the text that stands behind the input, its just a little decoration type of thing.
 
 
 if %ans%==1 (
@@ -131,6 +22,7 @@ goto F2
 if %ans%==3 (
 goto F3
 )
+rem Organization 1
 
 :F1
 cls
@@ -143,6 +35,7 @@ goto Fix2
 :F3
 cls
 goto Fix3
+rem Organization 2
 
 :Fix1
 
